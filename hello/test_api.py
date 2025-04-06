f0b61ebb64cc058a876c5c10e2c55090638f66c0
zkan <zkan.cs@gmail.com>
2025-04-06 20:25:14 +0700

    Add id func

--- hello/test_api.py
+++ hello/test_api.py
@@ -18,13 +18,17 @@ def test_exclamation():
     assert result.endswith("!")
 
 
+def idfn(x):
+    return f'"{x}"'
+
+
 @pytest.mark.parametrize("arg_str, expected", [
     ("-g Hey", "Hey, World!"),
     ("--greeting Hey", "Hey, World!"),
     ("-n Kan", "Hello, Kan!"),
     ("--nam Kan", "Hello, Kan!"),
     ("-g Hey -n Kan", "Hey, Kan!"),
-])
+], ids=idfn)
 def test_greeting(arg_str, expected):
     result = hello.full_output(arg_str)
     assert result == expected
